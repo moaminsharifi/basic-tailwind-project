8121ca39730c31aec5f3adc7609f5a431c3c615b
Amin Sharifi <moaminsharifiminabi@gmail.com>
2022-04-01 20:16:33 +0430

    basic files added to repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# basic tailwind project
+
+---
+### install packages:
+```
+npm install
+```
+
+### run project:
+```
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    content: ["public/index.html", "post.html", 'public/index copy.html'],
+  
+    theme: {
+      extend: {
+        zIndex: {
+        
+        },
+        height: {
+         
+        },
+        width: {
+         
+        },
+        bottom:{
+         
+          
+        },
+        fontFamily: {
+        
+        },
+        
+        borderWidth: {
+         
+        },
+        padding: {
+         
+        },
+  
+  
+        colors: {
+
+        },
+        backgroundImage: {
+         
+        },
+  
+        screens: {
+          xs: "200px",
+        },
+      },
+  
+    },
+    plugins: [],
+  };
